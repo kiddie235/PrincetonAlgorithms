80fb5ca7995fadec34ea60a7901705023bf077ff
kiddie235 <kiddie235@yahoo.om>
2016-03-09 09:48:23 -0800

    checked in required libraries

--- HelloAlgorithms.java
+++ HelloAlgorithms.java
@@ -3,5 +3,9 @@ public class HelloAlgorithms {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to the world of Algorithms!");
+		int[] a = {1, 2, 3, 4, 5};
+		int b = a[0];
+		a[0] = 10;
+		System.out.println(a[0]);
 	}
 }
